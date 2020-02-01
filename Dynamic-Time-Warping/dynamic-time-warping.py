0cbd9a1f0a15b4fed9a903e1da0d781c0f773c43
jeremy.zhang <zhangyue9306@gmail.com>
2020-02-01 14:05:52 +0800

    add py

---
+++ Dynamic-Time-Warping/dynamic-time-warping.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+def dtw(s, t):
+    n, m = len(s), len(t)
+    dtw_matrix = np.zeros((n+1, m+1))
+    for i in range(n+1):
+        for j in range(m+1):
+            dtw_matrix[i, j] = np.inf
+    dtw_matrix[0, 0] = 0
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            cost = abs(s[i-1] - t[j-1])
+            # take last min from a square box
+            last_min = np.min([dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1]])
+            dtw_matrix[i, j] = cost + last_min
+    return dtw_matrix
+
+
+def dtw2(s, t, window):
+    n, m = len(s), len(t)
+    w = np.max([window, abs(n-m)])
+    dtw_matrix = np.zeros((n+1, m+1))
+    
+    for i in range(n+1):
+        for j in range(m+1):
+            dtw_matrix[i, j] = np.inf
+    dtw_matrix[0, 0] = 0
+    
+    for i in range(1, n+1):
+        for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
+            dtw_matrix[i, j] = 0
+    
+    for i in range(1, n+1):
+        for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
+            cost = abs(s[i-1] - t[j-1])
+            # take last min from a square box
+            last_min = np.min([dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1]])
+            dtw_matrix[i, j] = cost + last_min
+    return dtw_matrix
